5026f42b2eb91925d9bb873a9eae4cc8267d0dcc
Dean Abner Julian <dabnerjulian@yahoo.com>
2025-01-26 23:19:01 +0700

    Updated To Laravel 9

--- app/Domains/Auth/Http/Controllers/Frontend/Auth/TwoFactorAuthenticationController.php
+++ app/Domains/Auth/Http/Controllers/Frontend/Auth/TwoFactorAuthenticationController.php
@@ -18,8 +18,8 @@ class TwoFactorAuthenticationController
         $secret = $request->user()->createTwoFactorAuth();
 
         return view('frontend.user.account.tabs.two-factor-authentication.enable')
-            ->withQrCode($secret->toQr())
-            ->withSecret($secret->toString());
+            ->with('qrCode', $secret->toQr())
+            ->with('secret', $secret->toString());
     }
 
     /**
@@ -29,7 +29,7 @@ class TwoFactorAuthenticationController
     public function show(Request $request)
     {
         return view('frontend.user.account.tabs.two-factor-authentication.recovery')
-            ->withRecoveryCodes($request->user()->getRecoveryCodes());
+            ->with('recoveryCodes', $request->user()->getRecoveryCodes());
     }
 
     /**
